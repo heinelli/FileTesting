7d664170df0298358f3e0307e5316f548980039b
Elliot Heiner <elliotheiner1997@gmail.com>
2021-11-11 09:01:22 -0700

    Add file manipulating code.

---
+++ FileTesting/FileTesting.vb
@@ -0,0 +1,39 @@
+﻿Module FileTesting
+
+    Sub Main()
+        Dim currentRecord As String
+
+
+        '    FileOpen(1, "sample.txt", OpenMode.Input)
+        '    'The number "1" is now the variable by which the text file is known in the program
+        '    'Append is used to add to a text file or create the text file
+        '    'Input is used to read the text file
+        '    'Output is used to rewrite a text file
+
+        '    'Commas work as delimiters in the text file.
+        '    Do Until EOF(1)
+        '        Input(1, currentRecord)
+        '        Console.WriteLine(currentRecord)
+        '    Loop
+
+
+        '    FileClose(1)
+        '    Console.ReadLine()
+
+
+
+
+        Dim record As String = "$$ABCApple,$$ABCPear"
+        Dim temp() As String
+        Dim cleanData() As String
+
+        temp = Split(record, ",")
+
+        cleanData = Split(temp(0), "$$ABC")
+        Console.WriteLine(cleanData(1))
+
+        Console.ReadLine()
+
+    End Sub
+
+End Module
