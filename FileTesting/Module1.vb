85019a328706c0517f78c8fe9dbbc9d3551f911d
Elliot Heiner <elliotheiner1997@gmail.com>
2021-11-11 08:38:21 -0700

    Add project files.

---
+++ FileTesting/Module1.vb
@@ -0,0 +1,26 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim currentRecord As String
+
+
+        FileOpen(1, "sample.txt", OpenMode.Input)
+        'The number "1" is now the variable by which the text file is known in the program
+        'Append is used to add to a text file or create the text file
+        'Input is used to read the text file
+        'Output is used to rewrite a text file
+
+        'Commas work as delimiters in the text file.
+        Do Until EOF(1)
+            Input(1, currentRecord)
+            Console.WriteLine(currentRecord)
+        Loop
+
+
+        FileClose(1)
+        Console.ReadLine()
+
+
+    End Sub
+
+End Module
